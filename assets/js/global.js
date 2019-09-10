c87c814f6c00379612053c64bf2c43cf436a3762
Nicolas Jouanno <nicolas.jouanno@gmail.com>
2019-09-10 12:16:05 +0200

    Update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,9 +1,9 @@
 // API KEY = AIzaSyBjxkfGwlThS3JGnh95mYG4vvEgXK4pxb0
-var file = https://sheets.googleapis.com/v4/spreadsheets/1AWIDHqtH7m6715XM7yhVqrOUumjJZzzPCBLwpvlSKAw/values/stages!2:1000?key=AIzaSyBjxkfGwlThS3JGnh95mYG4vvEgXK4pxb0
+var file = "https://sheets.googleapis.com/v4/spreadsheets/1AWIDHqtH7m6715XM7yhVqrOUumjJZzzPCBLwpvlSKAw/values/stages!2:1000?key=AIzaSyBjxkfGwlThS3JGnh95mYG4vvEgXK4pxb0"
 
 
 function stageComponent (name, startDate, endDate, location, link) {
-  "<div class=\"row\">" +
+  return "<div class=\"row my-3\">" +
     "<div class=\"col-12\">" +
       "<span>Du " + startDate + " au " + endDate + " à " + location + " → " + name + "</span>" +
       "<a href=\" " + link + " \" target=\"_blank\" rel=\"nofollow\" class=\"btn btn-secondary ml-5 rounded-pill shadow\">En savoir +</a>" +
@@ -14,9 +14,16 @@ function stageComponent (name, startDate, endDate, location, link) {
 function loadStage(file) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', file);
-    xhr.addEventListener('readystatechange', function() { // On gère ici une requête asynchrone
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { // Si le fichier est chargé sans erreur
-            document.getElementById('stages_list').innerHTML = '<span>' + xhr.responseText + '</span>'; // Et on affiche !
+    xhr.addEventListener('readystatechange', function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { // check no load file error 
+            var stages = JSON.parse(xhr.response).values // select values in response
+            var formateWithStageComponent = "" // initialization of the final display element
+            
+            for (var i = 0; i < stages.length; i++) { // build elements
+              formateWithStageComponent += stageComponent(stages[i][0],stages[i][1], stages[i][2], stages[i][3], stages[i][4])
+            }
+          
+            document.getElementById('stages_list').innerHTML = formateWithStageComponent; // Display elements
         }
     });
     xhr.send(null);
